5f0f9e3501808d949ec22d60e77292542234b5ba
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-07-28 21:14:19 -0300

    Validando horario do check-in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -7,6 +7,17 @@ export class InMemoryCheckInsRepository implements checkInsRepository {
 	// Criando um array de usuários vazio para simular o db
 	public items: CheckIn[] = []
 
+	async findById(id: string){
+		const checkIn = this.items.find((item) => item.id === id)
+
+		if (!checkIn) {
+			return null
+		}
+
+		return checkIn
+	}
+
+
 	async findByUserIdOnDate(userId: string, date: Date) {
 		const startOfTheDay = dayjs(date).startOf('day')
 		const endOfTheDay = dayjs(date).endOf('date')
@@ -47,6 +58,16 @@ export class InMemoryCheckInsRepository implements checkInsRepository {
 		}
 
 		this.items.push(checkIn)
+
+		return checkIn
+	}
+
+	async save(checkIn: CheckIn){
+		const checkInIndex = this.items.findIndex(item => item.id === checkIn.id)
+
+		if(checkInIndex >= 0){
+			this.items[checkInIndex] = checkIn
+		} 
 
 		return checkIn
 	}
