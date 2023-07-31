0e068726aaa8bf0204214c7ddf93f6c41ffa6063
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-07-31 18:18:08 -0300

    implementando JWT no Fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,9 +17,21 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 	try {
 		const authenticateUseCase = makeAuthenticateUseCase()
 
-		await authenticateUseCase.execute({
+		const { user } = await authenticateUseCase.execute({
 			email,
 			password,
+		})
+
+		const token = await reply.jwtSign(
+			{}, 
+			{
+				sign: {
+					sub: user.id,
+				}
+			}
+		)
+		return reply.status(200).send({
+			token,
 		})
 	} catch (err) {
 		if (err instanceof InvalidCredentialsError) {
@@ -28,5 +40,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 		throw err //TODO FIX ME
 	}
 
-	return reply.status(200).send()
 }
