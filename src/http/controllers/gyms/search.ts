ffbccc64a2ae21f0afb78e4c6a0a88aa607f8708
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-07-31 22:14:26 -0300

    Controllers das rotas de check-ins

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -11,7 +11,7 @@ export async function search(request: FastifyRequest, reply: FastifyReply){
 
 	})
 
-	const { q, page } = searchGymsQuerySchema.parse(request.body)
+	const { q, page } = searchGymsQuerySchema.parse(request.query)
 
 	
 	const searchGymsUseCase = makeSearchGymsUseCase()
